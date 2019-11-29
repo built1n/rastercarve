a2f106a69532385362993cb4e4c925c5c1eec29c
Franklin Wei <franklin@rockbox.org>
2019-11-29 18:37:15 -0500

    Add GPLv2+ license

--- src/rastercarve.py
+++ src/rastercarve.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
-"""rastercarve: a raster engraving G-code generator
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019 Franklin Wei
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+# KIND, either express or implied.
+
+"""
+rastercarve: a raster engraving G-code generator
+
 Usage: rastercarve.py IMAGE
 
 This program outputs G-code to engrave a bitmap image on a 3-axis
